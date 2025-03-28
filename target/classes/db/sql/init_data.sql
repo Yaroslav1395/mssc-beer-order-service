52b091bff913e14f2cafcdbf8028b60e84f7fe27
Yarnercom <a.doroev@mail.ru>
2025-03-28 19:39:08 +0600

    рефакторинг и комментарии

--- target/classes/db/sql/init_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO BEER_ORDER_SERVICE.customers (id, version, create_date, last_modified_date, customer_name, api_key)
-VALUES (
-    gen_random_uuid(),
-    0,
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP,
-    'Tasting Room',
-    gen_random_uuid()
-);
